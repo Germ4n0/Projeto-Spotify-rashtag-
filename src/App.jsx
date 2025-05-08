be0db97a2829d911c38822eb81765844977111b3
Guilherme Alves Soares <guilherme.soares@memora.com.br>
2025-05-08 13:57:37 -0300

    Primeiro commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// arrow function
+const App2 = () => {
+  console.log("pedro fudido");
+
+  return <h1>pedro fudido</h1>;
+}
+
+function App() {
+  console.log("pedro fudido");
+
+  return <h1>pedro fudido</h1>;
+}
+
+export default App
